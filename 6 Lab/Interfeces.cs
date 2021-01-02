3889fbb3c9b560dd8867fab3fe2d0e754d4183e2
Гиль Анастасия <70573584+NastyaLig@users.noreply.github.com>
2021-01-02 22:55:25 +0300

    Add files via upload

---
+++ 6 Lab/Interfeces.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _6_lab
+{
+    public interface IPaper : IBouquet
+    {
+        void PackIn();
+    }
+
+    public interface IBouquet
+    {
+        void Collect();
+    }
+
+    public interface IPot : IBouquet
+    {
+        void PutInPot();
+        void ToPlant();
+    }
+}
